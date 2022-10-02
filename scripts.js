d7504d8b95d2c58c061f0ebb13fc95190b5c9478
Nabil <holanabilkhalid@gmail.com>
2022-10-02 23:49:39 +0800

    Rename display variable and event handler function

--- scripts.js
+++ scripts.js
@@ -25,12 +25,12 @@ function operate(operatorChoice, num1, num2){
 
 console.log(operate(multiply, 10, 5))
 
-let displayValue = document.querySelector(".display");
+let display = document.querySelector(".display");
 let keypad = document.querySelectorAll(".keypad");
 keypad.forEach(pad => {
-    pad.addEventListener("click", clickPad);
-})
+    pad.addEventListener("click", calculation);
+});
 
-function clickPad(e) {
-   displayValue.textContent = e.target.textContent; 
-};
+function calculation(e) {
+    display.textContent = e.target.textContent; 
+}
